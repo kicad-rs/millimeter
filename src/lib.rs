a55e1e5b866530aea2f7e2788e8fed904cf3d31a
Dominic <git@msrd0.de>
2021-12-28 19:56:09 +0100

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,330 @@
+#![cfg_attr(feature = "const_float_classify", feature(const_float_classify))]
+#![allow(uncommon_codepoints)] 
+#![warn(rust_2018_idioms, unreachable_pub)]
+#![forbid(unsafe_code)]
+
+use paste::paste;
+#[cfg(feature = "serde")]
+use serde::{de::Error as _, Deserialize, Deserializer, Serialize, Serializer};
+use std::{
+	fmt::{self, Debug, Display, Formatter},
+	ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Neg, Sub, SubAssign},
+	str::FromStr
+};
+#[cfg(feature = "std")]
+use thiserror::Error;
+
+#[derive(Clone, Copy, Debug)]
+#[cfg_attr(feature = "std", derive(Error))]
+#[cfg_attr(feature = "std", error("non-finite number"))]
+pub struct NonFinite;
+
+macro_rules! unit {
+	(pub const struct $name:ident($inner:ident);) => {
+		unit!(@internal [const]; $name($inner););
+	};
+
+	(pub struct $name:ident($inner:ident);) => {
+		unit!(@internal []; $name($inner););
+	};
+
+	(@internal [$($const:tt)?]; $name:ident($inner:ident);) => {
+		paste! {
+			mod [<unit_ $name>] {
+				use super::NonFinite;
+
+				#[derive(Clone, Copy, PartialEq, PartialOrd)]
+				#[allow(non_camel_case_types)]
+				pub struct $name($inner);
+
+				impl $name {
+					/// This is a helper method for creating this type. You might
+					/// want to use the [`Unit`] trait if you don't need a `const`
+					/// function.
+					pub $($const)? fn try_new(inner: $inner) -> Result<Self, NonFinite> {
+						if !inner.is_finite() {
+							return Err(NonFinite);
+						}
+						Ok(Self(inner))
+					}
+
+					/// This is a helper method for creating this type. You might
+					/// want to use the [`Unit`] trait if you don't need a `const`
+					/// function.
+					///
+					/// ### Panics
+					///
+					/// This method panics if the inner value is non-finite
+					pub $($const)? fn new(inner: $inner) -> Self {
+						match Self::try_new(inner) {
+							Ok(unit) => unit,
+							Err(NonFinite) => panic!(concat!(
+								stringify!($name),
+								" only supports finite numbers"
+							))
+						}
+					}
+
+					/// Return the raw value.
+					pub const fn raw_value(self) -> $inner {
+						self.0
+					}
+				}
+			}
+			pub use [<unit_ $name>]::$name;
+		}
+
+		impl $name {
+			/// Returns the nearest integer to a number. Round half-way cases away
+			/// from `0.0`.
+			#[must_use = "method returns a new number and does not mutate the original value"]
+			pub fn round(self) -> Self {
+				Self::new(self.raw_value().round())
+			}
+		}
+
+		impl std::cmp::Eq for $name {}
+
+		impl std::cmp::Ord for $name {
+			fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+				// unwrap: Self will never store non-finite values, so all values
+				// should be comparable
+				self.partial_cmp(other).unwrap()
+			}
+		}
+
+		impl std::hash::Hash for $name {
+			fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+				std::hash::Hash::hash::<H>(&self.raw_value().to_bits(), state);
+			}
+		}
+
+		impl Neg for $name {
+			type Output = Self;
+			fn neg(self) -> Self {
+				Self::new(-self.raw_value())
+			}
+		}
+
+		impl Add for $name {
+			type Output = Self;
+			fn add(self, rhs: Self) -> Self {
+				Self::new(self.raw_value() + rhs.raw_value())
+			}
+		}
+
+		impl AddAssign for $name {
+			fn add_assign(&mut self, rhs: Self) {
+				*self = *self + rhs;
+			}
+		}
+
+		impl Sub for $name {
+			type Output = Self;
+			fn sub(self, rhs: Self) -> Self {
+				Self::new(self.raw_value() - rhs.raw_value())
+			}
+		}
+
+
+		impl SubAssign for $name {
+			fn sub_assign(&mut self, rhs: Self) {
+				*self = *self - rhs;
+			}
+		}
+
+		impl Mul<$inner> for $name {
+			type Output = Self;
+			fn mul(self, rhs: $inner) -> Self {
+				Self::new(self.raw_value() * rhs)
+			}
+		}
+
+		impl Mul<$name> for $inner {
+			type Output = $name;
+			fn mul(self, rhs: $name) -> $name {
+				$name::new(self * rhs.raw_value())
+			}
+		}
+
+		impl MulAssign<$inner> for $name {
+			fn mul_assign(&mut self, rhs: $inner) {
+				*self = *self * rhs;
+			}
+		}
+
+		impl Div<$inner> for $name {
+			type Output = Self;
+			fn div(self, rhs: $inner) -> Self {
+				if !rhs.is_finite() {
+					panic!("Division through non-finite number");
+				}
+				if rhs == 0.0 {
+					panic!("Division through zero");
+				}
+				Self::new(self.raw_value() / rhs)
+			}
+		}
+
+		impl DivAssign<$inner> for $name {
+			fn div_assign(&mut self, rhs: $inner) {
+				*self = *self / rhs;
+			}
+		}
+
+		impl FromStr for $name {
+			type Err = <$inner as FromStr>::Err;
+			fn from_str(s: &str) -> Result<Self, Self::Err> {
+				Ok(Self::new(s.parse()?))
+			}
+		}
+
+		impl Display for $name {
+			fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+				Display::fmt(&self.raw_value(), f)
+			}
+		}
+
+		impl Debug for $name {
+			fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+				Debug::fmt(&self.raw_value(), f)?;
+				f.write_str(stringify!($name))
+			}
+		}
+
+		impl<'de> Deserialize<'de> for $name {
+			fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+			where
+				D: Deserializer<'de>
+			{
+				$inner::deserialize(deserializer).and_then(|inner| {
+					Self::try_new(inner).map_err(|err| D::Error::custom(err))
+				})
+			}
+		}
+
+		impl Serialize for $name {
+			fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+			where
+				S: Serializer
+			{
+				self.raw_value().serialize(serializer)
+			}
+		}
+	};
+}
+
+macro_rules! square_unit {
+	(pub const struct $name:ident($inner:ident) = $base:ident^2;) => {
+		unit!(@internal [const]; $name($inner););
+		square_unit!(@internal $name($inner) = $base^2);
+	};
+
+	(pub struct $name:ident($inner:ident) = $base:ident^2;) => {
+		unit!(@internal []; $name($inner););
+		square_unit!(@internal $name($inner) = $base^2);
+	};
+
+	(@internal $name:ident($inner:ident) = $base:ident^2) => {
+		impl $name {
+			pub fn sqrt(self) -> $base {
+				$base::new(self.raw_value().sqrt())
+			}
+		}
+
+		impl Mul<$base> for $base {
+			type Output = $name;
+			fn mul(self, rhs: $base) -> $name {
+				$name::new(self.raw_value() * rhs.raw_value())
+			}
+		}
+
+		impl Div<$base> for $name {
+			type Output = $base;
+			fn div(self, rhs: $base) -> $base {
+				$base::new(self.raw_value() / rhs.raw_value())
+			}
+		}
+	};
+}
+
+#[cfg(not(feature = "const_float_classify"))]
+unit! {
+	pub struct mm(f32);
+}
+
+#[cfg(feature = "const_float_classify")]
+unit! {
+	pub const struct mm(f32);
+}
+
+#[cfg(not(feature = "const_float_classify"))]
+square_unit! {
+	pub struct mm2(f32) = mm^2;
+}
+
+#[cfg(feature = "const_float_classify")]
+square_unit! {
+	pub const struct mm2(f32) = mm^2;
+}
+
+macro_rules! unit_trait {
+	(pub trait $ident:ident {
+		$(fn $name:ident(self) -> $unit:ident $(= self * $convert:literal)? ;)*
+	}) => {
+		paste! {
+			mod [<private_ $ident:lower>] {
+				pub trait Sealed {}
+				impl Sealed for f32 {}
+			}
+
+			pub trait $ident: [<private_ $ident:lower>]::Sealed {
+				$(fn $name(self) -> $unit;)*
+			}
+
+			impl $ident for f32 {
+				$(unit_trait!(@internal fn $name(self) -> $unit $(= self * $convert)?);)*
+			}
+		}
+	};
+
+	(@internal fn $name:ident(self) -> $unit:ident) => {
+		fn $name(self) -> $unit {
+			$unit::new(self)
+		}
+	};
+
+	(@internal fn $name:ident(self) -> $unit:ident = self * $convert:literal) => {
+		fn $name(self) -> $unit {
+			$unit::new(self * $convert)
+		}
+	};
+}
+
+unit_trait! {
+	pub trait Unit {
+		fn nm(self) -> mm = self * 1e6;
+		fn nm2(self) -> mm2 = self * 1e12;
+		
+		fn µm(self) -> mm = self * 1e3;
+		fn µm2(self) -> mm2 = self * 1e6;
+		
+		fn mm(self) -> mm;
+		fn mm2(self) -> mm2;
+		
+		fn cm(self) -> mm = self * 1e-1;
+		fn cm2(self) -> mm2 = self * 1e-2;
+		
+		fn dm(self) -> mm = self * 1e-2;
+		fn dm2(self) -> mm2 = self * 1e-4;
+		
+		fn m(self) -> mm = self * 1e-3;
+		fn m2(self) -> mm2 = self * 1e-6;
+
+		fn km(self) -> mm = self * 1e-6;
+		fn km2(self) -> mm2 = self * 1e-12;
+
+		fn inch(self) -> mm = self * 2.54;
+		fn inch2(self) -> mm2 = self * 645.16;
+	}
+}
